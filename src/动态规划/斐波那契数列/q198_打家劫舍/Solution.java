c0162871dc601d493f55cf49594de3e127808bd2
audience7510 <audience7510@163.com>
2021-07-19 13:52:49 +0800

    动态规划

--- src/动态规划/斐波那契数列/q198_打家劫舍/Solution.java
+++ src/动态规划/斐波那契数列/q198_打家劫舍/Solution.java
@@ -10,7 +10,13 @@ package 动态规划.斐波那契数列.q198_打家劫舍;
  */
 public class Solution {
     public int rob(int[] nums) {
+        int pre = 0;
+        int next = 0;
+        for (int i = 0; i < nums.length; i++) {
 
+            int cur = Math.max(pre+nums[i],next);
+
+        }
 
         return 0;
     }
--- src/动态规划/斐波那契数列/q70_爬楼梯/Solution.java
+++ src/动态规划/斐波那契数列/q70_爬楼梯/Solution.java
@@ -8,6 +8,7 @@ package 动态规划.斐波那契数列.q70_爬楼梯;
  * @Description
  * 思路：第n个楼梯，可以从第n-1和n-2的楼梯一步到达，走到n其实是
  *      走到n-1和走到n-2的和
+ *      dp[i] = dp[i-1] + dp[i-2]
  */
 public class Solution {
     public int climbStairs(int n) {
@@ -18,8 +19,11 @@ public class Solution {
         if (n == 2){
             return 2;
         }
+        //一阶楼梯一种
         arr[1] = 1;
+        //两阶楼梯两种
         arr[2] = 2;
+        //3 = 2+1，4 = 3+2+1，以此类推
         for (int i = 3; i < arr.length; i++) {
             arr[i] = arr[i-1]+arr[i-2];
         }
