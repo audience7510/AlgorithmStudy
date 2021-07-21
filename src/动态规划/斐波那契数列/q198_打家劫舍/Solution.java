2a76515add41e860648a8a35bb11557a987b4f71
audience7510 <audience7510@163.com>
2021-07-21 11:16:26 +0800

    动态规划

--- src/动态规划/斐波那契数列/q198_打家劫舍/Solution.java
+++ src/动态规划/斐波那契数列/q198_打家劫舍/Solution.java
@@ -8,7 +8,7 @@ package 动态规划.斐波那契数列.q198_打家劫舍;
  * @Description
  * 思路：一共k间房子，偷第k间，那么不能偷k-1间，偷窃总金额为 前k-2间房最高总金额与第k间房金额之和
  *      不偷第k间，那么总金额为 前k-1间房最高总金额
- *      dp[i]=max(dp[i−2]+nums[i],dp[i−1])
+ *      状态转移方程：dp[i]=max(dp[i−2]+nums[i],dp[i−1])
  */
 public class Solution {
     /**
