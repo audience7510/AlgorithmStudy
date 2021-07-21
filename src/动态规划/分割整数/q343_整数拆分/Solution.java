e5fff295d1df14d603359bbefa286db560e12045
audience7510 <audience7510@163.com>
2021-07-21 15:45:42 +0800

    动态规划

---
+++ src/动态规划/分割整数/q343_整数拆分/Solution.java
@@ -0,0 +1,16 @@
+package 动态规划.分割整数.q343_整数拆分;
+
+/**
+ * @ClassName Solution
+ * @Author audience
+ * @Date 2021/7/21
+ * @Version 1.0
+ * @Description
+ */
+public class Solution {
+    public int integerBreak(int n) {
+
+
+        return 0;
+    }
+}
