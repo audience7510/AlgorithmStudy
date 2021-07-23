43921412baf2374f779c5c9c7fa755ea8931cae7
audience7510 <audience7510@163.com>
2021-07-23 17:09:02 +0800

    动态规划

---
+++ src/动态规划/最长递增子序列/q376_摆动序列/Solution.java
@@ -0,0 +1,16 @@
+package 动态规划.最长递增子序列.q376_摆动序列;
+
+/**
+ * @ClassName Solution
+ * @Author audience
+ * @Date 2021/7/23
+ * @Version 1.0
+ * @Description
+ */
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+
+
+        return 0;
+    }
+}
