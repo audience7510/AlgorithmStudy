c3205b8e459aca2afb736cc22a726f2f9f80e75a
audience7510 <audience7510@163.com>
2021-07-26 14:49:34 +0800

    动态规划

--- src/动态规划/最长递增子序列/q376_摆动序列/Solution.java
+++ src/动态规划/最长递增子序列/q376_摆动序列/Solution.java
@@ -9,8 +9,19 @@ package 动态规划.最长递增子序列.q376_摆动序列;
  */
 public class Solution {
     public int wiggleMaxLength(int[] nums) {
+        if (nums == null|| nums.length == 0){
+            return 0;
+        }
+        int up = 1;
+        int down = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>nums[i-1]){
+                up = down+1;
+            }else if (nums[i]<nums[i-1]){
+                down = up+1;
+            }
+        }
 
-
-        return 0;
+        return Math.max(up,down);
     }
 }
