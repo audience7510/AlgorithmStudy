39e1b5cf13183b29241e67eab5be0a06b177896c
audience7510 <audience7510@163.com>
2021-08-16 13:54:56 +0800

    栈和队列

---
+++ src/栈和队列/q496_下一个更大元素I/Solution.java
@@ -0,0 +1,17 @@
+package 栈和队列.q496_下一个更大元素I;
+
+/**
+ * @ClassName Solution
+ * @Author audience
+ * @Date 2021/8/16
+ * @Version 1.0
+ * @Description
+ * 思路：暴力匹配时间复杂度O(n^2)
+ */
+public class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+
+        return null;
+    }
+}
---
+++ src/栈和队列/q503_下一个更大元素II/Solution.java
@@ -0,0 +1,16 @@
+package 栈和队列.q503_下一个更大元素II;
+
+/**
+ * @ClassName Solution
+ * @Author audience
+ * @Date 2021/8/16
+ * @Version 1.0
+ * @Description
+ */
+public class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+
+        return null;
+    }
+}
