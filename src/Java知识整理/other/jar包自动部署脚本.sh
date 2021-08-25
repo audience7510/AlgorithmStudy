0554539d2b6c38ec185d84ec3726c3b3e9cc0028
audience7510 <audience7510@163.com>
2021-08-25 16:44:02 +0800

    部署脚本

--- src/Java知识整理/other/jar包自动部署脚本.sh
+++ src/Java知识整理/other/jar包自动部署脚本.sh
@@ -16,9 +16,13 @@ echo "将编译好的jar包拷贝到指定目录下(目录和jar包名自定义)
 \cp /home/services/javaresource/ciic-ihr-upms/ciic-ihr-upms-service/target/ciic-ihr-upms-service-6.0.1-SNAPSHOT.jar  /home/services/javajar/ciic-ihr-upms
 
 
-echo "已启动的服务进程关闭(服务名自定义)"
+echo "已启动的服务进程关闭(根据服务名kill进程)"
 
 kill -9 $(ps aux | grep ciic-ihr-upms | tr -s ' '| cut -d ' ' -f 2)
+
+echo "已启动的服务进程关闭(根据端口号kill进程)"
+
+kill -9 $(lsof -i:4098 |awk '{print $2}' | tail -n 2)
 
 sleep 5
 
