c6422afa8f019aab7a2224302283c8fd3ee97e2c
audience7510 <audience7510@163.com>
2021-09-02 11:14:52 +0800

    链表相关

--- src/链表/q21_合并两个有序链表/Solution.java
+++ src/链表/q21_合并两个有序链表/Solution.java
@@ -10,19 +10,25 @@ package 链表.q21_合并两个有序链表;
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        //递归调用结束，谁先为null，则说明另一个链表，升序的值较大
+        //两个链表为升序，递归调用结束，谁先为null，链表到末尾了
         if (l1 == null){
             return l2;
         }
         if (l2 == null){
             return l1;
         }
-        //递归调用结束，返回最后一个最大值，用第二大的节点 指向最大的节点
+        //若l1小于l2，继续传入l1的下一个节点与l2进行比较
+        //
         if (l1.val < l2.val){
+            //当出栈时，l1较小，就将较大的l2拼接到l1的后面
             l1.next = mergeTwoLists(l1.next, l2);
+            //l1拼接完之后返回，用于下次出栈
             return l1;
         }else {
+            //若l1大于等于l2，继续传入l2的下一个节点与l1进行比较
+            //出栈时，将较大的l1拼接到l2的后面
             l2.next = mergeTwoLists(l1,l2.next);
+            //l2拼接完之后返回，用于下次出栈
             return l2;
         }
     }
