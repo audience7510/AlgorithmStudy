6030ffea482589bb9e11715c13dd5d347cab1dd0
shiyongming <shiyongming>
2021-09-05 11:16:10 +0800

    树

--- src/树/递归/q236_二叉树的最近公共祖先/Solution.java
+++ src/树/递归/q236_二叉树的最近公共祖先/Solution.java
@@ -9,8 +9,20 @@ package 树.递归.q236_二叉树的最近公共祖先;
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-
-        return null;
+        if (root == null){
+            return root;
+        }
+        if (root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null){
+            return right;
+        }
+        if (right == null){
+            return left;
+        }
+        return root;
     }
 }
