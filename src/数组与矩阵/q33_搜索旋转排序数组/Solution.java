8f79f6ec58934d58ec99af51e46f8d20fde5c897
audience7510 <audience7510@163.com>
2021-09-06 15:52:50 +0800

    数组与矩阵

---
+++ src/数组与矩阵/q33_搜索旋转排序数组/Solution.java
@@ -0,0 +1,52 @@
+package 数组与矩阵.q33_搜索旋转排序数组;
+
+/**
+ * @ClassName Solution
+ * @Author audience
+ * @Date 2021/9/6
+ * @Version 1.0
+ * @Description
+ * 思路：二分查找法
+ *      1、数组中间分隔，判断那边有序
+ *      2、在有序的半段里，判断目标值在不在
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n<=1){
+            return nums[0] == target? nums[0]:-1;
+        }
+        int l = 0;
+        int r = n-1;
+        while (l<r){
+            int mid = (l+r)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            //数组左半部分是有序[4567123] 2
+            if (nums[0]<=nums[mid]){
+                //target在0和mid中间
+                if (nums[0]<=target && target<nums[mid]){
+                    //0到mid-1找target
+                    r = mid-1;
+                }else {
+                    //mid+1到length-1找target
+                    l = mid+1;
+                }
+            }
+            //右半部分是有序
+            else {
+                //target在mid和length-1之间
+                if (nums[mid+1]<target && target<=nums[n-1]){
+                    //在mid+1和length-1之间找target
+                    l = mid+1;
+                }else {
+                    //在0和mid-1之间找target
+                    r = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
