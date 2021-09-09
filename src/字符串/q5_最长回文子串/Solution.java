4ef69fa4b39ac3803b6c4d48a4c2868205fe2945
audience7510 <audience7510@163.com>
2021-09-09 10:31:11 +0800

    字符串

--- src/字符串/q5_最长回文子串/Solution.java
+++ src/字符串/q5_最长回文子串/Solution.java
@@ -17,27 +17,29 @@ public class Solution {
         }
         int start = 0;
         int end = 0;
-        String res = "";
         for (int i = 0; i < s.length(); i++) {
             //奇数 以i为回文中心，最长字符串
-            String s1 = expandAroundCenter(s,i,i);
+            int s1 = expandAroundCenter(s,i,i);
             //偶数 以i和i+1为回文中心，最长字符串
-            String s2 = expandAroundCenter(s,i,i+1);
-            res = res.length()>s1.length()?res:s1;
-            res = res.length()>s2.length()?res:s2;
+            int s2 = expandAroundCenter(s,i,i+1);
+            int len = Math.max(s1,s2);
+            if (len > end-start){
+                start = i - (len-1)/2;
+                end = i + len/2;
+            }
         }
-        return res;
+        return s.substring(start,end+1);
     }
 
-    public static String expandAroundCenter(String s, int left, int right) {
+    public static int expandAroundCenter(String s, int left, int right) {
         //索引在0和length之间，并且回文中心 两边相等，说明是回文串
         while (left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
             //向左和向右移动
             left--;
             right++;
         }
-        //最终返回回文字符串长度vadcdam 0+1=1 6-0-1=5
-        return s.substring(left+1,right-left-1);
+        //最终返回回文字符串长度  vadcdam 0+1=1 6-0-1=5
+        return right-left-1;
     }
 
     public static void main(String[] args) {
