b76fc00d52aeb1782ea6778acf83bbfec52c3339
audience7510 <audience7510@163.com>
2021-09-30 09:23:06 +0800

    链表

--- src/剑指offer/链表/JZ3从尾到头打印链表/Solution.java
+++ src/剑指offer/链表/JZ3从尾到头打印链表/Solution.java
@@ -1,6 +1,7 @@
 package 剑指offer.链表.JZ3从尾到头打印链表;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * @ClassName Solution
@@ -10,9 +11,41 @@ import java.util.ArrayList;
  * @Description
  */
 public class Solution {
+
+    ArrayList<Integer> list = new ArrayList<>();
+    Stack<ListNode> stack = new Stack();
+
+    /**
+     * @Date 2021/9/30
+     * @Param
+     * @return
+     * @Description 递归写法，牛客网运行会报栈内存溢出
+     */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-
-
-        return null;
+        if (listNode!=null){
+            printListFromTailToHead(listNode.next);
+            list.add(listNode.val);
+        }
+        return list;
     }
+
+    /**
+     * @Date 2021/9/30
+     * @Param [listNode]
+     * @return
+     * @Description 栈写法
+     */
+    public ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
+        while (listNode!=null){
+            stack.push(listNode);
+            listNode = listNode.next;
+        }
+
+        while (!stack.isEmpty()){
+            ListNode pop = stack.pop();
+            list.add(pop.val);
+        }
+        return list;
+    }
+
 }
