66c1a2a93ca1f3257751d5e6746eb728291d53c7
audience7510 <audience7510@163.com>
2021-10-12 15:04:51 +0800

    链表

--- src/LeetCode/链表/q61_旋转链表/Solution.java
+++ src/LeetCode/链表/q61_旋转链表/Solution.java
@@ -6,8 +6,9 @@ package LeetCode.链表.q61_旋转链表;
  * @Date 2021/7/9
  * @Version 1.0
  * @Description
- * 思路：先遍历求得链表总长度count，同时将链表首尾相连
- *      再找到原链表的倒数第k+1个节点，该节点的next就是新链表的头结点
+ * 思路：先遍历求得链表总长度count，同时将链表首尾相连成环
+ *      再找到原链表的最后节点，该节点next移动length-(k%length)节点，
+ *      断开链表尾结点与头节点相连，该节点就是新链表的头结点
  */
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
@@ -27,8 +28,9 @@ public class Solution {
         last = first;
         //连接成环
         first.next = head;
-        //假设链表为12345，k为2，k%length为2，loop为3
+        //假设链表为12345，k为2，k%length(2%5)，商0余2，loop为3
         int loop = length-(k%length);
+//        int loop = k+1;
         first = head;
         for (int i = 0; i < loop; i++) {
             first = first.next;
