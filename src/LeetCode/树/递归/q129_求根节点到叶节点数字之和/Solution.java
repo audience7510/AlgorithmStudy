3cd0d1f9cd217f9d3a45f5e3ae62a5727d042630
audience7510 <audience7510@163.com>
2021-11-02 10:51:14 +0800

    树

--- src/LeetCode/树/递归/q129_求根节点到叶节点数字之和/Solution.java
+++ src/LeetCode/树/递归/q129_求根节点到叶节点数字之和/Solution.java
@@ -6,7 +6,8 @@ package LeetCode.树.递归.q129_求根节点到叶节点数字之和;
  * @Date 2021/10/15
  * @Version 1.0
  * @Description
- * 思路：深度优先，从根节点开始，若遇到叶子节点，则相加返回，否则对节点进行递归
+ * 思路：深度优先，从根节点开始，上一次的值乘以10，再加上本节点的值
+ *      如果不存在左右节点，直接返回，否则返回递归的左右节点
  */
 public class Solution {
     public int sumNumbers(TreeNode root) {
@@ -18,7 +19,7 @@ public class Solution {
             return 0;
         }
         int sum = i * 10 + root.val;
-        if (root.left!=null && root.right!=null){
+        if (root.left==null && root.right==null){
             return sum;
         }else {
             return dfs(root.left,sum)+dfs(root.right,sum);
