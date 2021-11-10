f04cd3682c07da55deaf8fd60558a12f6864764c
audience7510 <audience7510@163.com>
2021-11-10 11:08:55 +0800

    链表

--- src/LeetCode/链表/q92_反转链表II/Solution.java
+++ src/LeetCode/链表/q92_反转链表II/Solution.java
@@ -13,8 +13,15 @@ package LeetCode.链表.q92_反转链表II;
  *         对于head.next.next，反转区间是left-2到right-2，以此类推
  *         直到left减为1了，head也移动到了left的位置，因为保持的left到right的区间不变
  *         所以已经变成了情况2
+ *
+ *      方式2使用迭代：切断left左边得到链表1，切断right右边的链表得到链表2，
+ *      同时得到left到right的链表3，
+ *      翻转链表3，再拼接到链表1和2中间
  */
 public class Solution {
+    /**
+     * 递归方式
+     */
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if(left == 1){
             //此方法是翻转前n个节点
@@ -40,5 +47,55 @@ public class Solution {
         //翻转之后，拼接上right==1 之后的节点
         head.next = res;
         return listNode;
+    }
+
+    /**
+     * 迭代方式
+     */
+    public ListNode reverseBetween2(ListNode head, int left, int right) {
+        //创建虚拟头节点
+        ListNode dHead = new ListNode(-1);
+        dHead.next = head;
+        //创建用于遍历的节点
+        ListNode cur = dHead;
+
+        //遍历得到left前一个节点，因为在切断的时候，前一个的next刚好left节点
+        for (int i = 0; i < left - 1; i++) {
+            cur = cur.next;
+        }
+
+        //用一个新变量 遍历得到right节点
+        ListNode rightNode = cur;
+        //遍历结束，此时rightNode就是right节点
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        //切断链表，切成三段
+        ListNode leftNode = cur.next;
+        ListNode last = rightNode.next;
+        cur.next = null;
+        rightNode.next = null;
+        //切完之后，得到cur、leftNode、last三段链表，leftNode是需要翻转的链表
+        //翻转leftNode
+        reverseN2(leftNode);
+
+        //翻转之后再拼接
+        //翻转之后，rightNode翻转到前面了，所以拼接到cur后面
+        cur.next = rightNode;
+        //翻转之后，leftNode翻转到后面了
+        leftNode.next = last;
+        return dHead.next;
+    }
+
+    private void reverseN2(ListNode leftNode) {
+        ListNode pre = null;
+        ListNode cur = leftNode;
+        while (cur!=null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
     }
 }
