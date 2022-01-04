497670f7f9d94bde0cffc60472df9ea2f94ea572
audience7510 <audience7510@163.com>
2022-01-04 18:08:56 +0800

    动态规划

--- src/LeetCode/动态规划/矩阵路径/q221_最大正方形/Solution.java
+++ src/LeetCode/动态规划/矩阵路径/q221_最大正方形/Solution.java
@@ -10,9 +10,6 @@ package LeetCode.动态规划.矩阵路径.q221_最大正方形;
  *      dp[i][j]表示以(i,j)为右下角，且只包含1的正方形边长最大值
  *      如果是(0,0)，那么dp[i][j] = 0，不能由1组成正方形
  *      如果该位置是1，那么由左边、上边、左上角三个相邻元素的最小值决定，并加上当前的1
- *      1、工资问题
- *      2、反馈情况，努力干之后的感受，
- *      3、表明态度
  */
 public class Solution {
     public int maximalSquare(char[][] matrix) {
@@ -22,13 +19,22 @@ public class Solution {
         }
         int row = matrix.length;
         int line = matrix[0].length;
+        //dp数组表示以(i,j)为右下角，且只包含1的正方形边长最大值
         int[][] dp = new int[row][line];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < line; j++) {
-
+                //只判断为 '1'的情况，若为'0' 则不存在于正方形的边长中
+                if (matrix[i][j] == '1'){
+                    if (i==0 || j==0){
+                        dp[i][j] = 1;
+                    }else {
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+                    }
+                    max = Math.max(dp[i][j],max);
+                }
             }
         }
-
-        return max;
+        int square = max * max;
+        return square;
     }
 }
